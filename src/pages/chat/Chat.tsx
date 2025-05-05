ca0d1dba2214c5b0e6942030245446eab149803a
Brunofell <brunofelimarti@gmail.com>
2025-05-05 09:42:33 -0300

    oie

---
+++ src/pages/chat/Chat.tsx
@@ -0,0 +1,8 @@
+export default function Chat(){
+    return (
+        <div>
+            <h1>Chat</h1>
+            <p>Aqui é a sala de chat.</p>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header";
+import React from "react";
+
+export default function Home(){
+    return (
+        <div>
+          <Header />
+          <div>
+            <h1>Home</h1>
+            <h2>Página inicial</h2>
+          </div>
+        </div>
+      );
+}
